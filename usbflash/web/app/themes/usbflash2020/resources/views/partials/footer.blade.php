685e7e42ea1d35e801f67acfaa58ccc589f01b13
Andrzej <andrzej@zelmandesign.com>
2020-10-22 14:08:11 +0200

    footer fix

--- usbflash/web/app/themes/usbflash2020/resources/views/partials/footer.blade.php
+++ usbflash/web/app/themes/usbflash2020/resources/views/partials/footer.blade.php
@@ -4,8 +4,10 @@
       <div class="row">
         <div class="col-md-3 col-sm-6">
           <strong>NAVIGATE</strong>
-          @php dynamic_sidebar('sidebar-footer')) @endphp
-        </div> 
+          @if (has_nav_menu('footer_navigation'))
+            {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'navbar-nav']) !!}
+          @endif
+        </div>
         <div class="col-md-4 col-sm-6 mb-3">
           <strong>CONNECT</strong>
           <div class="social-media">
@@ -23,7 +25,7 @@
           <strong>CONTACT</strong><br>
           @php the_field('address', 'option'); @endphp
           <br>
-          Tel:  @php the_field('telephone', 'option'); @endphp<br>
+          Tel: @php the_field('telephone', 'option'); @endphp<br>
           Fax: @php the_field('fax', 'option'); @endphp
         </div>  
       </div>
