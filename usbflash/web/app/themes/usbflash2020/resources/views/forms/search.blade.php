f030f487bf91d0b266fd98e0a6c112560c69c34f
Andrzej <andrzej@zelmandesign.com>
2020-11-02 11:56:52 +0100

    search bar css

--- usbflash/web/app/themes/usbflash2020/resources/views/forms/search.blade.php
+++ usbflash/web/app/themes/usbflash2020/resources/views/forms/search.blade.php
@@ -1,16 +1,20 @@
 <form role="search" method="get" class="search-form form-inline ml-auto" action="{{ home_url('/') }}">
-  <label>
-    <input class="form-control"
-      type="search"
-      class="search-field"
-      placeholder="{!! esc_attr_x('Search promo gifts &hellip;', 'placeholder', 'sage') !!}"
-      value="{{ get_search_query(array('usb-product', 'gadgets-product')) }}"
-      name="s"
-    >
-  </label>
-
-  <button type="submit" class="btn btn-danger" value="{{ esc_attr_x('', 'submit button', 'sage') }}">
-    <img src="@asset('images/Promo-Serach-Icon.svg')" alt="" width="20" height="20">
-  </button>
+  <div class="input-group">
+    <label>
+      <input class="form-control"
+        type="search"
+        class="form-control"
+        placeholder="{!! esc_attr_x('Search promo gifts &hellip;', 'placeholder', 'sage') !!}"
+        value="{{ get_search_query(array('usb-product', 'gadgets-product')) }}"
+        name="s"
+      >
+    </label>
+    <div class="input-group-append">
+      <button type="submit" class="btn btn-danger" value="{{ esc_attr_x('', 'submit button', 'sage') }}">
+        <img src="@asset('images/Promo-Serach-Icon.svg')" alt="" width="20" height="20">
+      </button>
+    </div>
+  </div>
 </form>
+
  
