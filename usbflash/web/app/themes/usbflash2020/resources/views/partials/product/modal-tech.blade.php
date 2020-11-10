3b6c5cd65d12845a1e08f7824d2795fbfc44858d
Andrzej <andrzej@zelmandesign.com>
2020-11-10 13:46:08 +0100

    forms

--- usbflash/web/app/themes/usbflash2020/resources/views/partials/product/modal-tech.blade.php
+++ usbflash/web/app/themes/usbflash2020/resources/views/partials/product/modal-tech.blade.php
@@ -27,67 +27,8 @@
             We aim to get price back to you within 1 hour, although during busy periods there may be a slightly longer wait
           </div>
          
-          <form>
-            <div class="form-group row">
-              <label for="recipient-name" class="col-sm-4 col-form-label mb-2 mb-md-0">Quantity</label>
-              <div class="col-sm-8 d-flex justify-content-center">
-                <div class="input-group">
-                  <input type="text" class="form-control" id="qty" placeholder="ex. 100" required> 
-                  <div class="qty-btn-like">QTY</div>
-                </div>
-              </div>
-            </div>
-            <fieldset class="form-group">
-              <div class="row">
-                <legend class="col-form-label col-sm-4 pt-0">How many print colours:</legend>
-                <div class="col-sm-8">
-                  <div class="form-check custom-control-inline">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-                    <label class="form-check-label" for="gridRadios1">
-                      1 col
-                    </label>
-                  </div>
-                  <div class="form-check custom-control-inline">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                    <label class="form-check-label" for="gridRadios2">
-                      2 col
-                    </label>
-                  </div>
-                  <div class="form-check custom-control-inline">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
-                    <label class="form-check-label" for="gridRadios3">
-                      3 col
-                    </label>
-                  </div>
-                  <div class="form-check custom-control-inline">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios4" value="option4">
-                    <label class="form-check-label" for="gridRadios4">
-                      4 col
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </fieldset>
-            <div class="form-group row">
-              <div class="col">
-                <input type="text" class="form-control" placeholder="Your Name" required>
-              </div>
-              <div class="col">
-                <input type="text" class="form-control" placeholder="Your Company Name">
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col">
-                <input type="text" class="form-control" id="staticEmail" value="" placeholder="Email Address" required>
-              </div>
-            </div>
-            <div class="form-group">
-              <textarea class="form-control" id="message-text" placeholder="Special Notes"></textarea>
-            </div>
-            <div class="form-group text-right">
-              <button class="btn btn-danger">send</button>
-            </div>
-          </form>
+          @shortcode('[contact-form-7 id="401" title="Tech Gifts Product"]')
+
         </div>
       </div>
     </div>
